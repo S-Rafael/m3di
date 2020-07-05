2374b2af10a97d4cb757c7605ddc90e97e74bd88
Rafael Siejakowski <rafal.siejakowski+git@gmail.com>
2020-07-05 14:12:29 -0300

    Move definitions of global constants, such as 'pi', to a separate file 'constants.h' and include this file from 'transcendental.h'

--- src/constants.h
+++ src/constants.h
@@ -2,30 +2,23 @@
  *   Copyright (C) 2019-2020 Rafael M. Siejakowski
  *   License information at the end of the file.
  */
-#ifndef __TRANSCENDENTAL_H__
-#define __TRANSCENDENTAL_H__
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
 
 #include <complex>
-#include <cfloat>
-#include <cmath>
-#include "constants.h"
+#include <limits>
 
 /*
-	Here we declare the key functions G_q, c used for the
-	numerical evaluation of the transcendental functions G_q(z) and c_q.
+	Constant definitions.
 */
 
-// Function declarations:
-CC G_q(CC q, CC z) noexcept;
-CC c(CC q) noexcept;
+// Type name aliases
+using CC = std::complex<double>;
 
-// Inline helper functions:
-inline CC square(CC z) {return z*z;};
-inline bool is_tiny(CC a)
-{
-	// This is a lot faster than complex abs()
-	return (std::abs(a.real()) < DBL_MIN && std::abs(a.imag()) < DBL_MIN);
-}
+// Compile-time constants:
+const CC INFTY = CC(std::numeric_limits<double>::infinity());
+constexpr double pi = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861;
+constexpr double twopi = 2.0*pi;
 
 #endif
 
