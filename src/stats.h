f9cc3c0b3363a67728ffbed17d6bfc251c538a7e
Rafael Siejakowski <rafal.siejakowski+git@gmail.com>
2021-04-01 13:57:20 -0300

    Add license information to stats.{h,cpp}

--- src/stats.h
+++ src/stats.h
@@ -1,3 +1,7 @@
+/*
+ *   Copyright (C) 2021 Rafael M. Siejakowski
+ *   License information at the end of the file.
+ */
 #ifndef __STATS_H__
 #define __STATS_H__
 
@@ -34,3 +38,23 @@ class stats
 };
 
 #endif
+/*
+ *
+ * Copyright (C) 2021 Rafael M. Siejakowski
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation;
+ * later versions of the GNU General Public Licence do NOT apply.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ */
